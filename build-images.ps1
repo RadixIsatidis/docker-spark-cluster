5ff652a20291974c44d3bcebbbb8b16bd8931b54
YanLeaon Lai <3702144+RadixIsatidis@users.noreply.github.com>
2020-05-20 20:29:02 +0800

    Update.

--- build-images.ps1
+++ build-images.ps1
@@ -1,4 +1,4 @@
-docker build -t spark-base:latest ./docker/base
-docker build -t spark-master:latest ./docker/spark-master
-docker build -t spark-worker:latest ./docker/spark-worker
-docker build -t spark-submit:latest ./docker/spark-submit
+docker build -t yanleaon/spark-base:latest ./docker/base
+docker build -t yanleaon/spark-master:latest ./docker/spark-master
+docker build -t yanleaon/spark-worker:latest ./docker/spark-worker
+docker build -t yanleaon/spark-submit:latest ./docker/spark-submit
